04fd745d2f093d8ad74f42c1a32c50bb434a238d
Michal Junek <dragonczz@gmail.com>
2022-06-21 16:20:55 +0200

    Refactory migrations to be up to code style, removes comments, adds typing

--- database/migrations/2021_09_30_074121_create_litters_table.php
+++ database/migrations/2021_09_30_074121_create_litters_table.php
@@ -6,14 +6,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+return new class extends Migration {
+    public function up(): void
     {
         Schema::create('litters', function (Blueprint $table) {
             $table->id();
@@ -32,12 +26,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::table('animals', function (Blueprint $table): void {
             $table->dropForeign('litter_id');
