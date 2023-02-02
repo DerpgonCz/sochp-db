25c0e3fa0a55fdff2c71cacfa1321a5d94dd1146
Michal Junek <dragonczz@gmail.com>
2023-02-02 17:03:34 +0100

    Adds return typehint to Flashes facade

--- app/Facades/Flashes.php
+++ app/Facades/Flashes.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class Flashes extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'flashes';
     }
