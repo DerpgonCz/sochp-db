44f6c4b18e45e8ea61ccc60afcbc240d94b13ad2
Michal Junek <michal@junek.pw>
2024-01-20 17:58:08 +0100

    Updates translations

--- app/Enums/Animal/AnimalFurEnum.php
+++ app/Enums/Animal/AnimalFurEnum.php
@@ -12,13 +12,13 @@ enum AnimalFurEnum: int
     use Localized;
     use Arrayable;
 
-    case FLAG_STANDARD = 0b00000001;
-    case FLAG_LONG_HAIRED = 0b00000010;
-    case FLAG_DOUBLE_REX = 0b00000100;
-    case FLAG_FUZZ = 0b00001000;
-    case FLAG_REX = 0b00010000;
-    case FLAG_SATIN = 0b00100000;
-    case FLAG_VELVETEEN = 0b01000000;
+    case FLAG_STANDARD = 0b1;
+    case FLAG_LONG_HAIRED = 0b10;
+    case FLAG_DOUBLE_REX = 0b100;
+    case FLAG_FUZZ = 0b1000;
+    case FLAG_REX = 0b10000;
+    case FLAG_SATIN = 0b100000;
+    case FLAG_VELVETEEN = 0b1000000;
     case FLAG_SILK = 0b10000000;
     case FLAG_SPHYNX = 0b100000000;
     case FLAG_PATCHWORK = 0b1000000000;
